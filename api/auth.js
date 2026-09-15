a0e0eedffa465fd7159a7739cb47c37aeca6c556
filthefrog <beriofilippo@gmail.com>
2026-09-15 12:04:32 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,5 +5,8 @@ export default function handler(req, res) {
   
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(SCOPE)}&access_type=offline&prompt=consent`;
   
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.status(200).json({ authUrl });
 }
+
+
