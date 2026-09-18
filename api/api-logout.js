329f7e632c8c512ca8ef29cbdc3d2cf6c448ba18
filthefrog <beriofilippo@gmail.com>
2026-09-18 11:24:51 +0200

    Delete api-logout.js

--- api/api-logout.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', [
-    'gmail_token=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0',
-    'gmail_refresh=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0'
-  ]);
-  res.status(200).json({ ok: true });
-}
